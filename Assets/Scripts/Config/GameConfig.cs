fb56fbdbb024eadfc863e88791bf5e859a92aa32
OrigamiGames <origamiproject69@gmail.com>
2023-11-28 14:52:54 +0530

    Update GameConfig.cs

--- Assets/Scripts/Config/GameConfig.cs
+++ Assets/Scripts/Config/GameConfig.cs
@@ -4,7 +4,7 @@
     {
         #region LevelConfig
 
-        public static int LEVEL_LENGTH = 6;
+        public static int LEVEL_LENGTH = 1; //6
         public static int U_CORNER_PROBABILITY = 5;
         public static float CAR_PASS_TRESHOLD = 10f;
         public static float U_CORNER_INTERACTIBLE = 27f;
